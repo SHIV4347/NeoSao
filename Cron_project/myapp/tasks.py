7c2446512e0366fac8abe6423127f018ba9bf587
SHIV4347 <shivprasad129806@gmail.com>
2025-12-18 11:03:06 +0530

    Update tasks.py

--- Cron_project/myapp/tasks.py
+++ Cron_project/myapp/tasks.py
@@ -7,7 +7,7 @@ def send_scheduled_email():
     subject = 'Hello from Django + Celery'
     message = 'This is a scheduled email sent by Celery.'
     from_email = settings.EMAIL_HOST_USER
-    recipient_list = ['shivprasad129806@gmail.com']  # your second mail
+    recipient_list = []  # list of emails 
 
     send_mail(subject, message, from_email, recipient_list)
     return "Email sent!"
